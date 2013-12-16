fed2f6047ea78ff267b61f114c70aea589862abf
Jaeho Shin <netj@sparcs.org>
2013-12-16 03:33:16 -0800

    bpm now compiles enabled plugins into one script
    that can be simply sourced at the load/login time.
    
    When a bash script that contains a `declare` is sourced inside a
    function, then unfortunately all of those declared variables become
    local variables, hence invisible outside the function.  For example,
    _xspecs variable in bash-completion2, sourced by bpm's bash_completion
    plugin was mysteriously disappearing after sourcing the defining script.
    (See: http://stackoverflow.com/q/2891473/390044)
    
    To avoid this variable capture by the internal functions of bpm, bpm now
    hijacks invocations of source (and `.`) to observe what new variables
    have been defined by the sourced script, and declares them explicitly as
    global.  Considering the values of variables slowed down the loading too
    much, so it's currently disabled.
    
    Somewhat related to this issue, but mostly independent, because bpm now
    compiles all plugins to be loaded into a single file, the loading part
    became much lighter.

--- bpm.sh
+++ bpm.sh
@@ -13,136 +13,87 @@
 #   bpm disable PLUGIN...
 #   bpm off PLUGIN...
 # 
-#   bpm load [PLUGIN...]
-#   bpm vocabularies
-# 
 #   bpm help
 #
 
+################################################################################
+
 BPM=${BASH_SOURCE:-$0}
 BPM_HOME=$(cd $(dirname "$BPM") &>/dev/null; pwd)
-BPM_TMPDIR=${BPM_TMPDIR:-$(d="${TMPDIR:-/tmp}/bpm-$USER"; mkdir -p "$d"; chmod go= "$d"; echo "$d")}
+BPM_TMPDIR=${BPM_TMPDIR:-$(
+        d="${TMPDIR:-/tmp}/bpm-$USER"
+        mkdir -p "$d"
+        chmod go= "$d"
+        echo "$d"
+    )}
+
+__bpm_msg() { echo bpm: "$@"; }
+__bpm_error() { __bpm_msg "$@" >&2; return 1; }
+if ${BPM_LOADED:-false}; then
+    __bpm_info() { __bpm_msg "$@"; }
+else
+    __bpm_info() { :; }
+fi
+
+################################################################################
+
+__bpm_list() {
+    local where=$1; shift
+    (
+    mkdir -p "$BPM_HOME"/"$where"
+    cd "$BPM_HOME"/"$where" &>/dev/null
+    [[ $# -gt 0 ]] || set -- *
+    eval command ls "$@" 2>/dev/null
+    )
+}
+
+__bpm_is() {
+    local t=$1; shift
+    local p=$1; shift
+    local negative_msg=${1:-}
+    local positive_msg=${2:-}
+    if [[ -e "$BPM_HOME"/$t/"$p" || -L "$BPM_HOME"/$t/"$p" ]]; then
+        [[ -z "$positive_msg" ]] || __bpm_error "$p: $positive_msg"
+    else
+        [[ -n "$positive_msg" ]] || __bpm_error "$p: ${negative_msg:-No such bash plug-in}"
+    fi
+}
+
+__bpm_plugin_info() {
+    local p=$1
+    local t="$p bash plug-in"
+    local bpm_hr1="================================================================================"
+    echo "$t"
+    echo "${bpm_hr1:0:${#t}}"
+    sed <"$BPM_HOME"/plugin/"$p" -n '
+    \@^#!/.*bash@, /^###*$/ {
+        /^# / s/^# //p
+        /^###*$/ q
+    }
+    '
+}
+
 bpm() {
     local Cmd=$1; shift
     local exitcode=0
 
-    msg() { echo bpm: "$@"; }
-    error() { msg "$@" >&2; return 1; }
-    if ${BPM_LOADED:-false}; then
-        info() { msg "$@"; }
-    else
-        info() { :; }
-    fi
-
-    local bpm_hr1="================================================================================"
-    local bpm_hr2="--------------------------------------------------------------------------------"
-
-    bpm_list() {
-        local where=$1; shift
-        (
-        mkdir -p "$BPM_HOME"/"$where"
-        cd "$BPM_HOME"/"$where" &>/dev/null
-        [ $# -gt 0 ] || set -- *
-        eval command ls "$@" 2>/dev/null
-        )
-    }
-
-    bpm_is() {
-        local t=$1; shift
-        local p=$1; shift
-        local negative_msg=${1:-}
-        local positive_msg=${2:-}
-        if [ -e "$BPM_HOME"/$t/"$p" -o -L "$BPM_HOME"/$t/"$p" ]; then
-            [ -z "$positive_msg" ] || error "$p: $positive_msg"
-        else
-            [ -n "$positive_msg" ] || error "$p: ${negative_msg:-No such bash plug-in}"
-        fi
-    }
-
-    bpm_info() {
-        local p=$1
-        local t="$p bash plug-in"
-        echo "$t"
-        echo "${bpm_hr1:0:${#t}}"
-        sed <"$BPM_HOME"/plugin/"$p" -n '
-        \@^#!/.*bash@, /^###*$/ {
-            /^# / s/^# //p
-            /^###*$/ q
-        }
-        '
-    }
-
-    bpm_load1() {
-        local bash_plugin=$1; shift
-        bash_plugin_load() { :; }
-        bash_plugin_login() { :; }
-        # source the plug-in
-        info "loading $bash_plugin"
-        . "$BPM_HOME"/plugin/"$bash_plugin"
-        # and load it
-        bash_plugin_load
-        # and load more for interactive login shells
-        [[ ${-//i/} = $- ]] || ! shopt -q login_shell || bash_plugin_login
-        unset bash_plugin_{load,login}
-    }
-    bpm_load() {
-        local p=
-        for p; do bpm_load1 "$p"; done
-    }
-
-    bpm_list_enabled_by_deps() {
-        (
-        cd "$BPM_HOME"/enabled &>/dev/null
-        local latest=$(command ls -tdL . * | head -n 1)
-        # echo $latest >&2
-        deps="$BPM_TMPDIR"/enabled.deps
-        if [ "$deps" -nt $latest ]; then
-            cat "$deps"
-        else
-            info "computing dependencies..." >&2
-            # analyze the "# Requires: " lines to order by dependencies
-            tmp=$(mktemp -d "$BPM_TMPDIR"/enabled.deps.XXXXXX)
-            trap 'rm -rf "$tmp"' EXIT
-            command ls | tee "$tmp"/more >"$tmp"/seen
-            while [ -s "$tmp"/more ]; do
-                # cat "$tmp"/more >&2; echo >&2
-                local ps=$(cat "$tmp"/more; : >"$tmp"/more)
-                for p in $ps; do
-                    local pf="$BPM_HOME"/plugin/"$p"
-                    [ -e "$pf" ] || { error "$p: Dangling plugin enabled"; continue; }
-                    for dep in $(sed -n '/^# Requires: / s/^# Requires: *//p' <"$pf"); do
-                        if ! grep -qxF "$dep" "$tmp"/seen; then
-                            bpm_is plugin "$dep" "Unknown plug-in required by $p" || continue
-                            echo "$dep" >>"$tmp"/more
-                            echo "$dep" >>"$tmp"/seen
-                        fi
-                        echo "$dep $p"
-                    done
-                    echo "$p" '*'
-                done
-            done | tsort | grep -v '^*$' |
-            tee "$deps"
-        fi
-        chmod go= "$deps"
-        )
-    }
-
     case $Cmd in
         find)
-            bpm_list plugin "$@" || bpm_list plugin "${@/%/*}"
+            __bpm_list plugin "$@" || __bpm_list plugin "${@/%/*}"
             ;;
 
         ls)
-            bpm_list enabled "$@" || bpm_list enabled "${@/%/*}"
+            __bpm_list enabled "$@" || __bpm_list enabled "${@/%/*}"
             ;;
 
         info)
             local first_info=true
             local p=
+            local bpm_hr2="--------------------------------------------------------------------------------"
             for p; do
-                bpm_is plugin "$p" || continue
+                __bpm_is plugin "$p" || continue
                 $first_info || { echo "$bpm_hr2"; echo; }; first_info=false
-                bpm_info "$p"
+                __bpm_plugin_info "$p"
             done
             ;;
 
@@ -151,10 +102,10 @@ bpm() {
             mkdir -p "$BPM_HOME"/enabled
             cd "$BPM_HOME"/enabled &>/dev/null
             for p; do
-                bpm_is plugin "$p" || continue
-                bpm_is enabled "$p" "" "Already enabled" || continue
+                __bpm_is plugin "$p" || continue
+                __bpm_is enabled "$p" "" "Already enabled" || continue
                 ln -sfn ../plugin/"$p"
-                msg "$p: Enabled"
+                __bpm_msg "$p: Enabled"
             done
             )
             ;;
@@ -164,21 +115,11 @@ bpm() {
             mkdir -p "$BPM_HOME"/enabled
             cd "$BPM_HOME"/enabled &>/dev/null
             for p; do
-                bpm_is enabled "$p" "Not enabled" || continue
+                __bpm_is enabled "$p" "Not enabled" || continue
                 unlink "$p"
-                msg "$p: Disabled"
+                __bpm_msg "$p: Disabled"
             done
             )
-            ;;
-
-        vocabularies) # load only vocabularies
-            bpm_load $(bpm_list plugin bpm.\*)
-            ;;
-
-        load) # load all plug-ins
-            [ $# -gt 0 ] || set -- $(bpm_list_enabled_by_deps)
-            bpm_load "$@"
-            BPM_LOADED=true
             ;;
 
         help|*)
@@ -187,11 +128,10 @@ bpm() {
             exitcode=2
             ;;
     esac
-    unset msg error info  bpm_is bpm_info bpm_list bpm_list_enabled_by_deps bpm_load bpm_load1
     return $exitcode
 }
 
-# bpm autocompletion
+# set up bpm autocompletion
 __bpmcomp() {
     local cur prev
     COMPREPLY=()
@@ -202,7 +142,7 @@ __bpmcomp() {
                 COMPREPLY=($(compgen -W "$(bpm find)" -- "$cur"))
                 ;;
             enable|on)
-                [ "$BPM_TMPDIR"/enabled.all -nt "$BPM_TMPDIR"/enabled.deps ] || { sort "$BPM_TMPDIR"/enabled.deps >"$BPM_TMPDIR"/enabled.all; chmod go= "$BPM_TMPDIR"/enabled.all; }
+                [[ "$BPM_TMPDIR"/enabled.all -nt "$BPM_TMPDIR"/enabled.deps ]] || { sort "$BPM_TMPDIR"/enabled.deps >"$BPM_TMPDIR"/enabled.all; chmod go= "$BPM_TMPDIR"/enabled.all; }
                 COMPREPLY=($(compgen -W "$(bpm find | comm -23 - "$BPM_TMPDIR"/enabled.all)" -- "$cur"))
                 ;;
             ls|disable|off)
@@ -210,14 +150,165 @@ __bpmcomp() {
                 ;;
         esac
     else
-        COMPREPLY=($(compgen -W "ls find info enable on disable off vocabularies load help" -- "$cur"))
+        COMPREPLY=($(compgen -W "ls find info enable on disable off help" -- "$cur"))
     fi
 }
 complete -F __bpmcomp bpm
 
-# with bpm vocabularies from now on...
-bpm vocabularies
-# source bpm run commands first
-! [ -r ~/.bpmrc ] || . ~/.bpmrc
-# then pass arguments to bpm if given
-[ $# -eq 0 ] || bpm "$@"
+################################################################################
+
+# compile all enabled plugins
+__bpm_list_enabled_by_deps() {
+    (
+    cd "$BPM_HOME"/enabled &>/dev/null
+    local latest=$(command ls -tdL . * | head -n 1)
+    # echo $latest >&2
+    deps="$BPM_TMPDIR"/enabled.deps
+    if [[ "$deps" -nt $latest ]]; then
+        cat "$deps"
+    else
+        __bpm_info "computing dependencies..." >&2
+        # analyze the "# Requires: " lines to order by dependencies
+        tmp=$(mktemp -d "$BPM_TMPDIR"/enabled.deps.XXXXXX)
+        trap 'rm -rf "$tmp"' EXIT
+        { echo bpm; command ls; } | tee "$tmp"/more >"$tmp"/seen
+        while [[ -s "$tmp"/more ]]; do
+            # cat "$tmp"/more >&2; echo >&2
+            local ps=$(cat "$tmp"/more; : >"$tmp"/more)
+            for p in $ps; do
+                local pf="$BPM_HOME"/plugin/"$p"
+                [[ -e "$pf" ]] || { __bpm_error "$p: Dangling plugin enabled"; continue; }
+                for dep in $(sed -n '/^# Requires: / s/^# Requires: *//p' <"$pf"); do
+                    if ! grep -qxF "$dep" "$tmp"/seen; then
+                        __bpm_is plugin "$dep" "Unknown plug-in required by $p" || continue
+                        echo "$dep" >>"$tmp"/more
+                        echo "$dep" >>"$tmp"/seen
+                    fi
+                    echo "$dep $p"
+                done
+                # make sure virtual plugin '*' depends on all others
+                echo "$p" '*'
+                # and all non-bpm plugins depend on the core bpm plugin
+                [[ $p = bpm.* || $p = bpm ]] || echo 'bpm' "$p"
+            done
+        done | tsort | grep -v '^[*]$' |
+        tee "$deps"
+    fi
+    chmod go= "$deps"
+    )
+}
+
+__bpm_compile() {
+    local script=$1; shift
+    {
+        echo '#!/usr/bin/env bash'
+        echo '# bash plugins compiled by bpm (https://github.com/netj/bpm)'
+    } >"$script"
+    mkdir -p "$BPM_TMPDIR"/compiled
+    local bash_plugin=
+    for bash_plugin; do
+        local compiled="$BPM_TMPDIR"/compiled/"$bash_plugin"
+        local src="$BPM_HOME"/plugin/"$bash_plugin"
+        if [[ "$src" -nt "$compiled" || "$BPM" -nt "$compiled" ]]; then
+            __bpm_info "compiling $bash_plugin" >&2
+            (
+            set -e
+            unset -f bash_plugin_load bash_plugin_login
+            # load the plugin
+            . "$src"
+            # and generate code
+            {
+                echo "# $bash_plugin"
+                echo '__bpm_load1() {'
+                echo 'local bash_plugin='\'"$bash_plugin"\'''
+                # parts for all shells
+                if type bash_plugin_load &>/dev/null; then
+                    type bash_plugin_load | tail -n +3
+                fi
+                # parts for interactive login shells
+                if type bash_plugin_login &>/dev/null; then
+                    echo 'if ! [[ ${-//i/} = $- ]] && shopt -q login_shell; then'
+                    type bash_plugin_login | tail -n +3
+                    echo 'fi'
+                fi
+                echo '}'
+                echo '__bpm_load1'
+            } >"$compiled"
+            ) || rm -f "$compiled"
+        fi
+        ! [[ -s "$compiled" ]] || cat "$compiled" >>"$script"
+    done
+}
+
+__bpm_compile_enabled() {
+    local script="$BPM_TMPDIR"/compiled.enabled.sh
+    if [[ "$script" -ot "$BPM_HOME"/plugin ||
+          "$script" -ot "$BPM_HOME"/enabled ||
+          "$script" -ot "$BPM" ]]; then
+        __bpm_info "updating $script" >&2
+        __bpm_compile "$script" $(__bpm_list_enabled_by_deps)
+    fi
+    echo "$script"
+}
+
+__bpm_compiled=$(__bpm_compile_enabled)
+unset -f __bpm_compile __bpm_compile_enabled __bpm_list_enabled_by_deps
+
+# prepare environment to source the compiled plugin load script
+unset -f builtin declare source
+__bpm_load1_declared_variables() {
+    declare -p | sh -c '
+        declare() {
+            printf "%s\t%s\t%s\n" "$1" "${2%%=*}" \
+# XXX not considering values changes of variables
+# "${2//[
+#]/\\n}"
+        }
+        builtin source /dev/stdin
+    ' | sort
+}
+__bpm_load1_hijack_source() {
+source() {
+    # track which variables have changed during source
+    __bpm_load1_declared_variables >"$BPM_TMPDIR"/vars.before
+    __bpm_load1_preserve_changed_vars() {
+        declare -p $(__bpm_load1_declared_variables |
+            comm -13 "$BPM_TMPDIR"/vars.before - |
+            cut -f2) >"$BPM_TMPDIR"/vars.sourced
+    }
+    __bpm_load1_source() {
+        unset -f source .  # pause hijacking uses of source/.
+        trap __bpm_load1_preserve_changed_vars RETURN
+        builtin source "$@"
+    }
+    __bpm_load1_source "$@"
+    trap - RETURN
+    # declare all changed variables as global
+    declare() { builtin declare -g "$@" 2>/dev/null; }
+    builtin source "$BPM_TMPDIR"/vars.sourced
+    unset -f declare \
+        __bpm_load1_source \
+        __bpm_load1_preserve_changed_vars \
+        #
+    __bpm_load1_hijack_source  # resume hijacking uses of source/.
+}
+.() { source "$@"; }
+}
+__bpm_load1_hijack_source
+
+# then source the plugins
+builtin source "$__bpm_compiled"
+BPM_LOADED=true
+
+# and restore the environment
+unset -v __bpm_compiled
+unset -f __bpm_load1 \
+    __bpm_load1_declared_variables \
+    __bpm_load1_hijack_source \
+    source . \
+    #
+
+################################################################################
+
+# finally, pass any arguments to bpm if given to the script
+[[ $# -eq 0 ]] || bpm "$@"
